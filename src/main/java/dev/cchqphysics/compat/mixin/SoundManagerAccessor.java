07d973edbcbf5c718905eb0e318e8c97e93e82ed
ztawfik523-lgtm <ztawfik523@gmail.com>
2026-09-04 09:20:10 +0300

    Reconstruct SoundManager accessor mixin

---
+++ src/main/java/dev/cchqphysics/compat/mixin/SoundManagerAccessor.java
@@ -0,0 +1,12 @@
+package dev.cchqphysics.compat.mixin;
+
+import net.minecraft.client.sounds.SoundEngine;
+import net.minecraft.client.sounds.SoundManager;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(SoundManager.class)
+public interface SoundManagerAccessor {
+    @Accessor("soundEngine")
+    SoundEngine cchqphysics$getSoundEngine();
+}
